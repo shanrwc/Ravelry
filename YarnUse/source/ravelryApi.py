57defc1fee196b4b29fab28dfa9d1793defd7739
shanrwc <shanrwc@gmail.com>
2015-03-20 15:43:05 +0100

    removed key

--- YarnUse/source/ravelryApi.py
+++
@@ -1,8 +0,0 @@
-import sys, urllib2, json, base64
-
-def queryRavelry(query):
-    request = urllib2.Request(query)
-    base64string = base64.encodestring('%s:%s' % ('DF2EF65A6DCD4910BC39','6NYcEbfAk6OGorac_zccaLp0vklTFTy8MXB1t6de')).replace('\n','')
-    request.add_header("Authorization","Basic %s" % base64string)
-    response = urllib2.urlopen(request)
-    return(response)
